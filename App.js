bacce532e65dee9ff259ae38f4790502d8272965
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-20 18:00:30 +0600

    implement safeareaView area view for android by statusbar.currentHeight

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{
+            backgroundColor: "red",
+            padding: 16,
+          }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "green", padding: 16 }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
   },
 });
