b6c7f264624c6a3f489cbfbecbf415a0fa02b872
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-30 02:01:21 +0600

    workig with the restaurent details page

--- src/components/restaurents/screens/restaurentDetails.screen.js
+++ src/components/restaurents/screens/restaurentDetails.screen.js
@@ -1,17 +1,69 @@
 /** @format */
 
-import { View, Text } from "react-native";
-import React from "react";
-import SafeAreaView from "react-native-safe-area-view";
-import RestaurentCard from "../components/restaurentCard.component";
+import React, { useState } from "react";
+import { SafeAreaView, View, ScrollView, Text } from "react-native";
+import { List } from "react-native-paper";
+import { RestaurentCard } from "../components/restaurentCard.component";
 
 export const RestaurentDetails = ({ route }) => {
+  const [breakfastExtended, setBreakfastExtended] = useState(false);
+  const [lunchExtended, setLunchExtended] = useState(false);
+  const [dinnerExtended, setDinnerExtended] = useState(false);
+  const [drinksExtended, setDrinksExtended] = useState(false);
   const { restaurent } = route.params;
+
+  const renderAccordion = (title, icon, extended, setExtended, items) => {
+    return (
+      <View>
+        <List.Accordion
+          title={title}
+          left={(props) => <List.Icon {...props} icon={icon} />}
+          expanded={extended}
+          onPress={() => setExtended(!extended)}>
+          {items.map((item, index) => (
+            <List.Item key={index} title={item} />
+          ))}
+        </List.Accordion>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView>
-      <View style={{ padding: 16 }}>
-        <RestaurentCard restaurent={restaurent} />
-      </View>
+      <RestaurentCard restaurent={restaurent} />
+
+      <ScrollView style={{ height: "100%" }}>
+        {renderAccordion(
+          "Breakfast",
+          "bread-slice",
+          breakfastExtended,
+          setBreakfastExtended,
+          ["Burger", "Salad", "Soup", "Steak"]
+        )}
+
+        {renderAccordion(
+          "Lunch",
+          "hamburger",
+          lunchExtended,
+          setLunchExtended,
+          ["Burger", "Salad", "Soup", "Steak"]
+        )}
+
+        {renderAccordion(
+          "Dinner",
+          "food-variant",
+          dinnerExtended,
+          setDinnerExtended,
+          ["Pasta", "Salad", "Soup", "Steak"]
+        )}
+
+        {renderAccordion("Drinks", "cup", drinksExtended, setDrinksExtended, [
+          "Coffee",
+          "Juice",
+          "Tea",
+          "Beer",
+        ])}
+      </ScrollView>
     </SafeAreaView>
   );
 };
